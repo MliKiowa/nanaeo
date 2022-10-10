9b6940f300e83dbdc4052657ef78df040fd464b8
MliKiowa <nineto0@163.com>
2022-10-10 14:06:00 +0800

    Create Dependent.sh

---
+++ script/Dependent.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# ------------------------------------------------------------------------------
+# nanaeo 部署脚本
+# @author Mlikiowa
+# @since 2022/10/10
+# ------------------------------------------------------------------------------
+
+npm install hexo-cli -g
+npm install
+git clone https://github.com/MliKiowa/BlogFriend
+cd BlogFriend          
+mv test/friend.yml ../source/_data/friends.yml
+cd ..
+cat source/_data/friends.yml
+rm -rf BlogFriend

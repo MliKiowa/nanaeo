0663f8dccb5499b643a4e7be2d38a8c4b7ffa069
MliKiowa <nineto0@163.com>
2022-10-10 18:51:45 +0800

    Update Deploy.sh

--- script/Deploy.sh
+++ script/Deploy.sh
@@ -1,11 +1,14 @@
-cd public
-git config --global init.defaultBranch main
-git init
 GITHUB_URL=https://oauth:${GITHUB_TOKEN}@github.com/Mlikiowa/Mlikiowa.github.io.git
+git clone ${GITHUB_URL} temp
+rm -rf temp/*
+mv public/* temp
+cd temp 
+git config --global init.defaultBranch main
+git 
 git remote add origin ${GITHUB_URL}
 git branch -M main
 git config --global user.name "Mlikiowa"
 git config --global user.email "nineto0@163.com"
-git add .
+git add *
 git commit -m "Automatic generation"
-git push --set-upstream origin main -f 
+git push --set-upstream origin main

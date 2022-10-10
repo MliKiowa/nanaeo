c09b446dff404ae3fcc1341df6dc895a2af05fa7
MliKiowa <nineto0@163.com>
2022-10-10 14:43:24 +0800

    Update Deploy.sh

--- script/Deploy.sh
+++ script/Deploy.sh
@@ -8,5 +8,4 @@ git config --global user.name "Mlikiowa"
 git config --global user.email "nineto0@163.com"
 git add .
 git commit -m "Automatic generation"
-git push --set-upstream origin main
-git push -f 
+git push --set-upstream origin main -f 

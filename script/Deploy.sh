75269cfea303a95cdf157e07fb73a73b9a3b1f60
MliKiowa <nineto0@163.com>
2022-10-10 14:36:28 +0800

    Update Deploy.sh

--- script/Deploy.sh
+++ script/Deploy.sh
@@ -2,6 +2,9 @@ cd public
 git init
 GITHUB_URL=https://oauth:${GITHUB_TOKEN}@github.com/Mlikiowa/Mlikiowa.github.io.git
 git remote add origin ${GITHUB_URL}
+git branch -M main
+git config --global user.name "Mlikiowa"
+git config --global user.email "nineto0@163.com"
 git add .
 git commit -m "Automatic generation"
 git push -f 

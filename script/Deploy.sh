e2a14c6bac5b2f494521a39bb81c021f544ad8f6
手瓜一十雪 <nanaeonn@outlook.com>
2023-12-19 14:33:37 +0800

    Update Deploy.sh

--- script/Deploy.sh
+++ script/Deploy.sh
@@ -1,7 +1,7 @@
 GITHUB_URL=https://oauth:${GITHUB_TOKEN}@github.com/Mlikiowa/Mlikiowa.github.io.git
 git clone ${GITHUB_URL} temp
 rm -rf temp/*
-mv public/* temp
+cp -r public/* temp
 cd temp 
 git config --global init.defaultBranch main
 git remote add origin ${GITHUB_URL}

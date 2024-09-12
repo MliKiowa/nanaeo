a1da1f99515856b200d2629e7507c1a134ef880e
手瓜一十雪 <nanaeonn@outlook.com>
2024-09-12 17:09:41 +0800

    Update volantis-sw.js

--- source/volantis-sw.js
+++ source/volantis-sw.js
@@ -9,7 +9,7 @@ const PreCachlist = [
 ];
 let NPMMirror = true;
 const NPMPackage = "@mlikiowa/nanaeo";
-let NPMPackageVersion = "1.0.1721720099796";
+let NPMPackageVersion = "1.0.1725627924716";
 let debug = true;
 // location.hostname == 'localhost' && (debug = true) && (NPMMirror = false);
 const handleFetch = async (event) => {
